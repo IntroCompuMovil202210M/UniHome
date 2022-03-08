8491cb6eafa848ef4f080f95b5fc71911471661a
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-03-07 23:20:19 -0500

    Actividad Registro terminada
    Se termino la Actividad de Registro

--- UniHome/app/src/main/java/com/example/unihome/MainActivity.java
+++ UniHome/app/src/main/java/com/example/unihome/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.unihome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botonRegistroMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        botonRegistroMain = findViewById(R.id.botonRegistrar);
+
+        botonRegistroMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent actividadRegistro = new Intent(v.getContext(),Registrar.class);
+                startActivity(actividadRegistro);
+            }
+        });
+
     }
 }
--- UniHome/app/src/main/java/com/example/unihome/Registrar.java
+++ UniHome/app/src/main/java/com/example/unihome/Registrar.java
@@ -3,12 +3,14 @@ package com.example.unihome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class Registrar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_registrar);
     }
 }
