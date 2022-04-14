14d5fe1d5f5b4dd0a15aaf735c9de93090fc3a32
Luis Marquinez <83611688+LuisM-cpu@users.noreply.github.com>
2022-04-13 22:21:23 -0500

    Resubida
    Se vuelve a crear el proyecto, se agrega el mapa y se solucionan algunos fallos de funcionamiento.

---
+++ UniHome/app/src/main/java/com/netteam/unihome/Inicio.java
@@ -0,0 +1,41 @@
+package com.netteam.unihome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class Inicio extends AppCompatActivity {
+
+    Button botonRegistroMain,botonEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_inicio);
+
+        botonRegistroMain = findViewById(R.id.botonRegistrar);
+        botonEntrar = findViewById(R.id.botonEntrar);
+
+        botonRegistroMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent actividadRegistro = new Intent(v.getContext(),Registrar.class);
+                startActivity(actividadRegistro);
+            }
+        });
+
+        botonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent actividadPrincipal = new Intent(v.getContext(),Principal.class);
+                startActivity(actividadPrincipal);
+            }
+        });
+
+    }
+}
