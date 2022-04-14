3ffc561c32953d74d78da713722b9c29e105b374
Luis Marquinez <83611688+LuisM-cpu@users.noreply.github.com>
2022-04-13 22:35:01 -0500

    Permiso de ubicación
    Se solicita el permiso de ubicación y se agrega un marcador al mapa usando la ubicación actual.

--- UniHome/app/src/main/java/com/netteam/unihome/Principal.java
+++ UniHome/app/src/main/java/com/netteam/unihome/Principal.java
@@ -1,51 +1,88 @@
 package com.netteam.unihome;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.netteam.unihome.databinding.ActivityPrincipalBinding;
 
 public class Principal extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityPrincipalBinding binding;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LatLng ubicacion;
+    private MarkerOptions marcador;
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        marcador = new MarkerOptions();
+        ubicacion = new LatLng(0,0);
+
         binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        solicitarPermisoUbicacion.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(this,ubicacionObtenida);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        marcador.position(ubicacion).title("Ubicacion Actual");
+        mMap.addMarker(marcador);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
     }
+
+    ActivityResultLauncher<String> solicitarPermisoUbicacion = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+                @SuppressLint("MissingPermission")
+                @Override
+                public void onActivityResult(Boolean result) {
+                    if(result == true){
+                        mFusedLocationClient.getLastLocation().addOnSuccessListener(Principal.this,ubicacionObtenida);
+                    }else{
+                        Toast.makeText(Principal.this, "No se ha otorgado el permiso para la ubicacion.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+    private OnSuccessListener<Location> ubicacionObtenida = new OnSuccessListener<Location>() {
+        @Override
+        public void onSuccess(Location location) {
+            if (location != null) {
+                ubicacion = new LatLng(location.getLatitude(),location.getLongitude());
+                onMapReady(mMap);
+            }
+        }
+    };
 }
