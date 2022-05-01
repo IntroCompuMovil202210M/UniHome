826559548c3efcdffc24245a9235a09f07c04409
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-05-01 12:09:05 -0500

    Maps Implementado
    Se agrega la api de maps al proyecto y se realiza la verificacion de que funciona
    
    Co-Authored-By: Daniel Castellanos <78168171+R4DRHunt@users.noreply.github.com>
    Co-Authored-By: mejiarocha <70533112+mejiarocha@users.noreply.github.com>
    Co-Authored-By: Luis Marquinez <83611688+LuisM-cpu@users.noreply.github.com>

--- UniHome/app/src/main/java/com/netteam/unihome/Principal.java
+++ UniHome/app/src/main/java/com/netteam/unihome/Principal.java
@@ -1,14 +1,92 @@
 package com.netteam.unihome;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class Principal extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.netteam.unihome.databinding.ActivityPrincipalBinding;
 
+public class Principal extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityPrincipalBinding binding;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LatLng ubicacion;
+    private MarkerOptions marcador;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal);
+
+        marcador = new MarkerOptions();
+        ubicacion = new LatLng(0,0);
+
+        binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        solicitarPermisoUbicacion.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(this,ubicacionObtenida);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        marcador.position(ubicacion).title("Ubicacion Actual");
+        mMap.addMarker(marcador);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    ActivityResultLauncher<String> solicitarPermisoUbicacion = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+                @SuppressLint("MissingPermission")
+                @Override
+                public void onActivityResult(Boolean result) {
+                    if(result == true){
+                        mFusedLocationClient.getLastLocation().addOnSuccessListener(Principal.this,ubicacionObtenida);
+                    }else{
+                        if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION))
+                        {
+                            Toast.makeText(Principal.this, "No se ha otorgado el permiso para la ubicacion.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+
+    private OnSuccessListener<Location> ubicacionObtenida = new OnSuccessListener<Location>() {
+        @Override
+        public void onSuccess(Location location) {
+            if (location != null) {
+                ubicacion = new LatLng(location.getLatitude(),location.getLongitude());
+                onMapReady(mMap);
+            }
+        }
+    };
 }
