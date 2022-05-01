8bb8bdc75f31bd42f8bfad45093e1885002b4de8
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-05-01 11:28:25 -0500

    Se creo de CERO Unihome para conectividad con Firebase
    Se creo el proyecto de CERO para garantizar funcionalidad con Firebase
    SE creo un proyecto DIFERENTE en Firebase llamado Unihome 2
    
    Co-Authored-By: Daniel Castellanos <78168171+R4DRHunt@users.noreply.github.com>
    Co-Authored-By: mejiarocha <70533112+mejiarocha@users.noreply.github.com>

--- UniHome/app/src/main/java/com/netteam/unihome/MainActivity.java
+++ UniHome/app/src/main/java/com/netteam/unihome/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,7 +18,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Inicio extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     Button botonRegistroMain,botonEntrar;
     EditText correo, contrasena;
@@ -28,8 +27,7 @@ public class Inicio extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_inicio);
+        setContentView(R.layout.activity_main);
 
         botonRegistroMain = findViewById(R.id.botonRegistrar);
         botonEntrar = findViewById(R.id.botonEntrar);
@@ -65,6 +63,7 @@ public class Inicio extends AppCompatActivity {
                     }else{
                         String error = task.getException().getMessage();
                         Log.i("INFO",error);
+                        Toast.makeText(MainActivity.this,error,Toast.LENGTH_LONG).show();
                     }
                 }
             });
@@ -116,6 +115,13 @@ public class Inicio extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = autenticacion.getCurrentUser();
+        updateUI(currentUser);
+    }
+
     private void updateUI(FirebaseUser usuarioActual){
         if(usuarioActual != null){
             Intent actividadInicio = new Intent(this,Principal.class);
@@ -125,75 +131,4 @@ public class Inicio extends AppCompatActivity {
             contrasena.setText("");
         }
     }
-
-
-    /*private View.OnClickListener registrarse = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent actividadRegistro = new Intent(view.getContext(),Registrar.class);
-            startActivity(actividadRegistro);
-        }
-    };
-
-    private View.OnClickListener iniciarSesion = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if(validateForm()){
-                login(correo.getText().toString(),contrasena.getText().toString());
-            }else{
-                Toast.makeText(Inicio.this, "Ingrese todos datos para iniciar sesión.", Toast.LENGTH_SHORT).show();
-            }
-        }
-    };
-
-    private boolean validateForm() {
-        boolean valid = true;
-        String email = correo.getText().toString();
-        if (TextUtils.isEmpty(email)) {
-            valid = false;
-        }
-        String password = contrasena.getText().toString();
-        if (TextUtils.isEmpty(password)) {
-            valid = false;
-        }
-        return valid;
-    }
-
-    private void login(String email, String password) {
-        if (validateForm()) {
-            autenticacion.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(Inicio.this, "Login satisfactorio.", Toast.LENGTH_SHORT).show();
-                                FirebaseUser user = autenticacion.getCurrentUser();
-                                updateUI(user);
-                            } else {
-                                Toast.makeText(Inicio.this, "Login fallido.", Toast.LENGTH_SHORT).show();
-                                updateUI(null);
-                            }
-                        }
-                    });
-        }
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = autenticacion.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    private void updateUI(FirebaseUser currentUser){
-        if(currentUser!=null){
-            Intent intent = new Intent(Inicio.this, Principal.class);
-            intent.putExtra("user", currentUser.getEmail());
-            startActivity(intent);
-        } else {
-            correo.setText("");
-            contrasena.setText("");
-        }
-    }*/
 }
