811354622fa9c11269d558de3ae73e9904a4f90d
Luis Marquinez <83611688+LuisM-cpu@users.noreply.github.com>
2022-05-03 23:01:49 -0500

    Actualizaciones de GPS
    Se implementa el servicio de seguimiento de GPS.
    
    Co-Authored-By: OscarFalla <60487872+OscarFalla@users.noreply.github.com>
    Co-Authored-By: Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
    Co-Authored-By: Daniel Castellanos <78168171+R4DRHunt@users.noreply.github.com>
    Co-Authored-By: mejiarocha <70533112+mejiarocha@users.noreply.github.com>

--- UniHome/app/src/main/java/com/netteam/unihome/PrincipalEstudiante.java
+++ UniHome/app/src/main/java/com/netteam/unihome/PrincipalEstudiante.java
@@ -1,13 +1,18 @@
 package com.netteam.unihome;
 
+import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,15 +22,26 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.netteam.unihome.databinding.ActivityPrincipalBinding;
 import com.netteam.unihome.databinding.ActivityPrincipalEstudianteBinding;
@@ -39,6 +55,9 @@ public class PrincipalEstudiante extends FragmentActivity implements OnMapReadyC
     private MarkerOptions marcador;
     private ImageButton cuentaE, chatsE, publicarE;
     private FirebaseAuth autenticacion;
+    private boolean settingsOK;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
 
     @SuppressLint("MissingPermission")
     @Override
@@ -49,6 +68,7 @@ public class PrincipalEstudiante extends FragmentActivity implements OnMapReadyC
 
         marcador = new MarkerOptions();
         ubicacion = new LatLng(0,0);
+        settingsOK = false;
 
         cuentaE = findViewById(R.id.cuentaE);
         chatsE = findViewById(R.id.chatsE);
@@ -61,6 +81,11 @@ public class PrincipalEstudiante extends FragmentActivity implements OnMapReadyC
         solicitarPermisoUbicacion.launch(Manifest.permission.ACCESS_FINE_LOCATION);
 
         mFusedLocationClient.getLastLocation().addOnSuccessListener(this,ubicacionObtenida);
+        solicitarPermisoUbicacion.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(this,ubicacionObtenida);
+        mLocationRequest = createLocationRequest();
+        mLocationCallback = callbackUbicacion;
+        checkLocationSettings();
 
         SupportMapFragment mapFragment2 = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapE);
@@ -78,6 +103,108 @@ public class PrincipalEstudiante extends FragmentActivity implements OnMapReadyC
         mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
         mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    ActivityResultLauncher<IntentSenderRequest> getLocationSettings =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartIntentSenderForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            Toast.makeText(PrincipalEstudiante.this, result.getResultCode(), Toast.LENGTH_SHORT).show();
+                            if(result.getResultCode() == RESULT_OK){
+                                settingsOK = true;
+                                startLocationUpdates();
+                            }else{
+                                Toast.makeText(PrincipalEstudiante.this, "GPS Apagado", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+    private void checkLocationSettings(){
+        LocationSettingsRequest.Builder builder = new
+                LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                settingsOK = true;
+                startLocationUpdates();
+            }
+        });
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(((ApiException) e).getStatusCode() == CommonStatusCodes.RESOLUTION_REQUIRED){
+                    ResolvableApiException resolvable = (ResolvableApiException) e;
+                    IntentSenderRequest isr = new IntentSenderRequest.Builder(resolvable.getResolution()).build();
+                    getLocationSettings.launch(isr);
+                }else {
+                    Toast.makeText(PrincipalEstudiante.this, "No hay GPS", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private LocationRequest createLocationRequest(){
+        LocationRequest locationRequest = LocationRequest.create()
+                .setInterval(10000)
+                .setFastestInterval(5000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+        }
+    }
+
+    private void stopLocationUpdates(){
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    private LocationCallback callbackUbicacion = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            Location nuevaUbicacion = locationResult.getLastLocation();
+            ubicacion = new LatLng(nuevaUbicacion.getLatitude(),nuevaUbicacion.getLongitude());
+            marcador.position(ubicacion).title("Ubicacion Actual");
+            Log.i("DB","Ubicación Actualizada!");
+            /*if(nuevaUbicacion!=null)
+            {
+                if(distance(ubicacion.latitude,ubicacion.longitude,
+                        nuevaUbicacion.getLatitude(),nuevaUbicacion.getLongitude()) >= 0.03)
+                {
+                    writeJSONObject();
+                    ubicacion = new LatLng(nuevaUbicacion.getLatitude(),nuevaUbicacion.getLongitude());
+                    marcador.position(ubicacion).title("Ubicacion Actual");
+                    mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+                }else{
+                    ubicacion = new LatLng(nuevaUbicacion.getLatitude(),nuevaUbicacion.getLongitude());
+                    marcador.position(ubicacion).title("Ubicacion Actual");
+                }
+
+            }*/
+        }
+    };
+
 
     ActivityResultLauncher<String> solicitarPermisoUbicacion = registerForActivityResult(
             new ActivityResultContracts.RequestPermission(),
