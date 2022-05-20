684e366f56d2882f304c72bedffcf19f2ebbfe2d
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-05-20 16:54:47 -0500

    Corrección login

---
+++ UniHome/app/src/main/java/com/netteam/unihome/Launcher.java
@@ -0,0 +1,92 @@
+package com.netteam.unihome;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Launcher extends AppCompatActivity {
+
+    FirebaseAuth autenticacion;
+    FirebaseUser usuario;
+    FirebaseFirestore db;
+    Arrendatario arrendatario;
+    Estudiante estudiante;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        autenticacion = FirebaseAuth.getInstance();
+        usuario = autenticacion.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+
+        verificarUsuario();
+
+
+    }
+    void verificarUsuario() {
+        if(usuario == null) {
+            startActivity(new Intent(Launcher.this, MainActivity.class));
+        } else {
+            buscarArrendatario(usuario.getUid());
+        }
+    }
+
+    private void buscarArrendatario(String id){
+        DocumentReference docRef = db.collection("arrendatarios").document(id);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        arrendatario = document.toObject(Arrendatario.class);
+                        Toast.makeText(Launcher.this, "Bienvenido/a "+arrendatario.getNombre(), Toast.LENGTH_SHORT).show();
+                        Intent iniciarArrendatario = new Intent(Launcher.this, PrincipalArrendatario.class);
+                        iniciarArrendatario.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(iniciarArrendatario);
+                    } else {
+                        buscarEstudiante(id);
+                    }
+                } else {
+                    Log.i("BD", "Excepción: "+ task.getException());
+                }
+            }
+        });
+    }
+
+    private void buscarEstudiante(String id){
+        DocumentReference docRef = db.collection("estudiantes").document(id);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        estudiante = document.toObject(Estudiante.class);
+                        Toast.makeText(Launcher.this, "Bienvenido/a "+estudiante.getNombre(), Toast.LENGTH_SHORT).show();
+                        Intent iniciarEstudiante = new Intent(Launcher.this, PrincipalEstudiante.class);
+                        iniciarEstudiante.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(iniciarEstudiante);
+                    } else {
+                        //Toast.makeText(MainActivity.this, "No existe el estudiante", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Log.i("BD", "Excepción: "+ task.getException());
+                }
+            }
+        });
+    }
+}
