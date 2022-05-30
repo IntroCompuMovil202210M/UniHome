10f7a717926337de36438c459b4f74a4353c845c
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-05-30 12:03:04 -0500

    Layouts de mansaje ok
    Se implementaron las vistas para el chat, falta implementación

--- UniHome/app/src/main/java/com/netteam/unihome/ListaChat.java
+++ UniHome/app/src/main/java/com/netteam/unihome/ListaChat.java
@@ -1,14 +1,31 @@
 package com.netteam.unihome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ListaChat extends AppCompatActivity {
+
+    private TextView usuarioChat;
+    private ImageView fotoPerfilChat;
+    private EditText mensajeChat;
+    private Button enviarMensaje;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_chat);
+
+        usuarioChat = findViewById(R.id.usuarioNombreChat);
+        fotoPerfilChat = findViewById(R.id.fotoPerfilChat);
+        mensajeChat = findViewById(R.id.escribirMensajeChat);
+        enviarMensaje = findViewById(R.id.enviarChat);
+        recyclerView = findViewById(R.id.recycleChats);
     }
 }
